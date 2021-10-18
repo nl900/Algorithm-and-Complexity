5dc175ce017c3cd54b5c523ff50139257a83cb86
NLee <49750572+nl900@users.noreply.github.com>
2021-10-18 15:03:41 +0200

    Update Knapsack-Problem.py

--- Knapsack-Problem.py
+++ Knapsack-Problem.py
@@ -1,3 +1,10 @@
 """
-Given weights and values of n items, put these items in a knacpsack with a capacity of C
+Given weights and values of n items, put these items in a knacpsack with a capacity of C.
+Find a subset of items that will maximise total value in the knapsack where the total weight
+is not more than C.
+
+The brute-force recursive solution is to try all combinations of items and choose the one with
+maximum profit where the weight does not exceed C.
 """
+
+def knapSack():
