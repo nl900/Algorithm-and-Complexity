fc442bd7746b690218a869c4e62b0833b71147a8
NLee <49750572+nl900@users.noreply.github.com>
2021-10-18 10:29:37 +0200

    Update Merge-Sort.py

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -4,4 +4,9 @@ and then finally combines the solutions to the subproblems to solve the original
 each subproblem must be smaller than the original problem and there must be a base case for subproblems.
 
 Merge sort employ divide and conquer based on recursion with running time of O(nlogn).
+It works likes this:
+1) Divide: find the midpoint of the array
+2) Conquer: sort the subarrays created in the divide step. Recursively sort the subarray, that is, if it has more than 2 elements
+and it's no the base case, then it is divided into subarrays again until base case of size 1.
+3) Combine: merge the 2 sorted subarrays back into a single sorted subarray.
 """
