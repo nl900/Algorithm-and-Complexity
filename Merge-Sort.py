1b76e420687572fb6c4134593c288894e8f7661d
NLee <49750572+nl900@users.noreply.github.com>
2021-10-18 11:00:41 +0200

    Update Merge-Sort.py

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -5,11 +5,44 @@ each subproblem must be smaller than the original problem and there must be a ba
 
 Merge sort employ divide and conquer based on recursion.
 It works likes this:
-1) Divide: find the midpoint of the array
-2) Conquer: sort the subarrays created in the divide step. Recursively sort the subarrays (call mergesort twice on the two subarrays),
-if it has more than 2 elements and it's no the base case, then it is divided into subarrays again until base case of size 1.
-3) Combine: merge the 2 sorted subarrays back into a single sorted subarray.
+1) Divide: find the midpoint of the array and divide into halves
+2) Conquer: if it's only 1 element, then it's already sorted, return. Otherwise, call mergesort on the two subarrays.
+3) Combine: merge the smaller lists in sorted order.
 
 Running time O(nlogn) as it divides the array in two halves and takes linear time to merge two halves.
 Space O(n) for the temporary array
 """
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2 # Find midpoint
+  
+        # Divide into 2 halves
+        L = arr[:mid]
+        R = arr[mid:]
+ 
+        mergeSort(L) # Sorting the first half
+        mergeSort(R) # Sorting the second half
+  
+        i = j = k = 0
+    
+        # Copy data to temp arrays
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+  
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+  
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
