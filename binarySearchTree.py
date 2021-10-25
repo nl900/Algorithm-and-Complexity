710beb4512fb167b219c2b0507499b827903d8cd
NLee <49750572+nl900@users.noreply.github.com>
2021-10-25 11:08:45 +0200

    Create binarySearchTree.py

---
+++ binarySearchTree.py
@@ -0,0 +1,13 @@
+"""
+A binary tree is a linked data structure where each nodes points to at most 2 child nodes.
+Nodes with no children are leaf nodes.
+
+The depth of a node is the number of nodes from root to the node, with root having depth
+of 0.
+The height of a node is the number of nodes on the path from the node to the deepest leaf.
+Count from the node and up towards the root.
+Height of the tree is the maximum height of any node in the tree.
+
+Binary Search Tree is a binary tree to help search data in the tree where the nodes greater
+than the root is on the right and nodes less than the rot is on the right.
+"""
