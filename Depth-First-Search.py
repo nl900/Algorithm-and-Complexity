2453cc2483f9c160e294ec98a366b6d89d32eab8
NLee <49750572+nl900@users.noreply.github.com>
2021-10-26 15:43:27 +0200

    Update Depth-First-Search.py

--- Depth-First-Search.py
+++ Depth-First-Search.py
@@ -3,14 +3,13 @@ Traversal on graph or tree structures. It explores as far as possible down each 
 backtracking.
 
 Time complexity on average is O(V+E) or O(n+m)on a graph when represented as an adjacency list (higher if using a list), 
-V=n is the number of vertices and E=m is the number of edges.
+V is the number of vertices and E is the number of edges.
 On a tree, it's O(V).
 A tree is a subset of graphs has N nodes and (N-1) edges and all nodes must be reachable from the 
 root and exactly one possible path from the root to the node.
 Graphs have no rules its connections and may contain any set of edges that can connect nodes in any way.
 
-Space complexity O(bm), b is the max branching factor and m is the max depth of the state space.
-Needing to store b nodes for each of the m nodes.
+Space complexity O(V)
 """
 
 visited = set() # Set to keep track of visited nodes.
