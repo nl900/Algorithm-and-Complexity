ece57e72716237490dcd40a3dbe51d32a9ffac95
NLee <49750572+nl900@users.noreply.github.com>
2021-10-26 15:13:35 +0200

    Create Depth-First-Search.py

---
+++ Depth-First-Search.py
@@ -0,0 +1,10 @@
+"""
+Traversal on graph or tree structures.
+
+Time complexity is O(V+E) on a graph when represented as an adjacency list (higher if using a list), 
+V is the number of vertices and E is the number of edges.
+On a tree, it's O(V).
+A tree is a subset of graphs has N nodes and (N-1) edges and all nodes must be reachable from the 
+root and exactly one possible path from the root to the node.
+Graphs have no rules its connections and may contain any set of edges that can connect nodes in any way.
+"""
