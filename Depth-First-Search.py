a9d9d75e3c1b212c65561af668a36ffe970ab7a0
NLee <49750572+nl900@users.noreply.github.com>
2021-10-26 15:27:24 +0200

    Update Depth-First-Search.py

--- Depth-First-Search.py
+++ Depth-First-Search.py
@@ -2,10 +2,22 @@
 Traversal on graph or tree structures. It explores as far as possible down each branch before
 backtracking.
 
-Time complexity is O(V+E) on a graph when represented as an adjacency list (higher if using a list), 
-V is the number of vertices and E is the number of edges.
+Time complexity on average is O(V+E) or O(n+m)on a graph when represented as an adjacency list (higher if using a list), 
+V=n is the number of vertices and E=m is the number of edges.
 On a tree, it's O(V).
 A tree is a subset of graphs has N nodes and (N-1) edges and all nodes must be reachable from the 
 root and exactly one possible path from the root to the node.
 Graphs have no rules its connections and may contain any set of edges that can connect nodes in any way.
+
+Space complexity O(bm), b is the max branching factor and m is the max depth of the state space.
+Needing to store b nodes for each of the m nodes.
 """
+
+visited = set() # Set to keep track of visited nodes.
+
+def recurs_dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            recurs_dfs(visited, graph, neighbour)
