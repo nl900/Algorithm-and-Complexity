d591abf6efc56235fe08d8c9fd0f7fce3979870f
NLee <49750572+nl900@users.noreply.github.com>
2021-11-08 12:22:19 +0100

    Update Merge-Sort.py

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -1,6 +1,7 @@
 """
-Divide and conquer is an algorithmic paradigm that breaks a problem into non-overlapping subproblems and recursively solves the subproblems 
-and then finally combines the solutions to the subproblems to solve the original problem. Since they're solved recursively,
+Divide and conquer is an algorithmic paradigm that breaks a problem into non-overlapping subproblems 
+and recursively solves the subproblems and then finally combines the solutions to the subproblems 
+to solve the original problem. Since they're solved recursively,
 each subproblem must be smaller than the original problem and there must be a base case for subproblems.
 
 Merge sort employ divide and conquer based on recursion.
